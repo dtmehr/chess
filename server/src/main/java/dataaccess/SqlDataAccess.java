5d3549a0c1ed5d614a65d4ef9f1d94a46700b603
Todd Mehr <dtmehr@gmail.com>
2025-03-11 12:15:35 -1000

    fixed configureDatabase, added tables statements (minor general errors)

--- server/src/main/java/dataaccess/SqlDataAccess.java
+++ server/src/main/java/dataaccess/SqlDataAccess.java
@@ -1,25 +1,52 @@
 package dataaccess;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import passoff.exception.ResponseParseException;
 import spark.Response;
 
 import java.sql.SQLException;
 
 public class SqlDataAccess implements DataAccess{
-    public SqlDataAccess() throws ResponseException {
+    private final Gson gson = new GsonBuilder().create();
 
+    public SqlDataAccess() throws DataAccessException {
+        configureDatabase();
     }
 
-    private void configureDatabase() throws ResponseException{
+    private final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS user(
+            username VARCHAR(255) NOT NULL,
+            password VARCHAR(255) NOT NULL,
+            PRIMARY KEY (username)
+            
+            CREATE TABLE IF NOT EXISTS `auth` (
+              token    VARCHAR(255) NOT NULL,
+              username VARCHAR(255) NOT NULL,
+              PRIMARY KEY (token),
+              FOREIGN KEY (username) REFERENCES `user` (username)
+                ON DELETE CASCADE
+                ON UPDATE CASCADE
+            
+            CREATE TABLE IF NOT EXISTS `game` (
+              game_id   INT NOT NULL AUTO_INCREMENT,
+              game_json TEXT NOT NULL,
+              PRIMARY KEY (game_id)
+            """
+
+    };
+
+    private void configureDatabase() throws DataAccessException{
         DatabaseManager.createDatabase();
         try (var conn = DatabaseManager.getConnection()){
             for (var statement : createStatements) {
-                try (var preparedStatement = con.prepareStatement(statement)) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
                     preparedStatement.executeUpdate();
                 }
             }
         } catch (SQLException exception){
-            throw new ResponseException (500, String.format("Unable to configure database %s", exception));
+            throw new DataAccessException ("Unable to configure database: " + exception.getMessage());
         }
 
     }
